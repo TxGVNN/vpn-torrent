f951d34734560f5263f85fb2417636bcf7f33d33
Giap Tran <txgvnn@gmail.com>
2018-05-18 10:37:47 +0700

    Support DNS checking

---
+++ README.md
@@ -0,0 +1,26 @@
+# vpn-torrent
+
+Sometimes, I need to download some resources via torrent, and I usually use VPN connection for this, but I don't want to connect directly VPN in the host system.
+
+So this is the idea from that.
+
+## Usage
+- If you like me, just for downloading via torrent
+
+```
+docker run -it --rm --cap-add=NET_ADMIN --device /dev/net/tun -v ~/Downloads:/data txgvnn/vpn-torrent rtorrent 'magnet:address' ...
+```
+
+Notes
+
+```
+-v ~/Downloads:/data - The /data is workdir, so files will be saved ~/Downloads
+-e COUNTRY=US        - The selected country for VPN server (Default is automatic)
+```
+
+- You want more than just only rtorrent
+
+You should rewrite the `Dockerfile` or `entrypoint.sh` for your purpose
+
+## Thanks for
+`Adhityaa` - Author of a [simple tool](https://github.com/adtac/autovpn) to automatically connect to VPN
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,23 +1,37 @@
 #!/bin/sh
 COUNTRY=${COUNTRY:-""}
 
+reconnect(){
+    if ! screen -ls >& /dev/null; then
+        echo "E: Cound not connect any VPN servers in $COUNTRY"
+        exit 1
+    fi
+    screen -S vpn -p 0 -X stuff "^Cy^M"
+}
 echo "COUNTRY: $COUNTRY"
 echo "I: Wait for VPN connection"
 screen -d -m -S vpn /app "$COUNTRY"
 
-sleep 15
-while ! ip a | grep tun >& /dev/null; do
-    if ! screen -ls >& /dev/null; then
-        echo "E: Cound not connect any VPN server in $COUNTRY"
-        exit 1
+while true; do
+    sleep 10
+    if ! ip a | grep tun >& /dev/null; then
+        echo "W: Try new server"
+        reconnect
+        continue
     fi
-    screen -S vpn -p 0 -X stuff "y^M"
-    echo "I: Try new the server"
-    sleep 15
+
+    echo "I: Check the DNS"
+    if ! nslookup github.com >& /dev/null; then
+        echo "W: DNS is not working. Try new server"
+        reconnect
+        continue
+    fi
+    echo "I: DNS is good"
+    break
 done
 
 echo "I: The VPN is ready!"
-sleep 5
 wget -qO- http://ipinfo.io
 sleep 1
+printf "\nI: Execute %s" "$1"
 exec "$@"
